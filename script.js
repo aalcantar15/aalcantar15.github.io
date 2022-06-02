43d0be8944d25956d8e933d525f69b5fd7f7f652
aalcantar15 <106777879+aalcantar15@users.noreply.github.com>
2022-06-02 22:09:47 +0000

    spacing

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ var end = Date.now() + duration;
 
 (function frame() {
   // launch a few confetti from the left edge 
- confetti({
-   particleCount: 7,
-   angle: 60,
-   spread: 55, 
-   origin: { x: 0 }
-});
-// and launch a few confetti from the right edge 
-confetti({
-  particleCount: 7,
-  angle: 120,
-  spread: 55,
-  origin: { x: 1 }
-});
+    confetti({
+      particleCount: 7,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 }
+    });
+    // and launch a few confetti from the right edge 
+    confetti({
+      particleCount: 7,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 }
+    });
 
 // keep going until we are out of time 
 if (Date.now() < end) { 
